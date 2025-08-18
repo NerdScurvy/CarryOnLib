df44f91f146f182ae3415c3be12781dbeba7ae8c
NerdScurvy <NerdScurvy@gmail.com>
2025-08-19 01:41:46 +0800

    Add new items to interface

--- src/API/Common/ICarryableTransfer.cs
+++ src/API/Common/ICarryableTransfer.cs
@@ -5,14 +5,17 @@ namespace CarryOn.API.Common
 {
     public interface ICarryableTransfer
     {
-        bool CanPutCarryable(IPlayer player, BlockEntity blockEntity, int index, ItemStack itemStack, TreeAttribute blockEntityData, out string failureCode, out string onScreenErrorMessage);
+        bool IsTransferEnabled(ICoreAPI api);
 
-        bool CanTakeCarryable(IPlayer player, BlockEntity blockEntity, int index, out string failureCode, out string onScreenErrorMessage);
+        bool CanPutCarryable(IPlayer player, BlockEntity blockEntity, int index, ItemStack itemStack, ITreeAttribute blockEntityData, out float? transferDelay, out string failureCode, out string onScreenErrorMessage);
 
-        bool TryPutCarryable(IPlayer player, BlockEntity blockEntity, int index, ItemStack itemstack, TreeAttribute blockEntityData, out string failureCode, out string onScreenErrorMessage);
+        bool CanTakeCarryable(IPlayer player, BlockEntity blockEntity, int index, out float? transferDelay, out string failureCode, out string onScreenErrorMessage);
 
-        bool TryTakeCarryable(IPlayer player, BlockEntity blockEntity, int index, out ItemStack itemstack, out TreeAttribute blockEntityData, out string failureCode, out string onScreenErrorMessage);
+        bool TryPutCarryable(IPlayer player, BlockEntity blockEntity, int index, ItemStack itemstack, ITreeAttribute blockEntityData, out string failureCode, out string onScreenErrorMessage);
 
-        bool BlockCarryAllowed(IPlayer player, BlockSelection selection);
+        bool TryTakeCarryable(IPlayer player, BlockEntity blockEntity, int index, out ItemStack itemstack, out ITreeAttribute blockEntityData, out string failureCode, out string onScreenErrorMessage);
+
+        bool IsBlockCarryAllowed(IPlayer player, BlockSelection selection);
+
     }
 }
