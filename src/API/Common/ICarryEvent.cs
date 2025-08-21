8ccc43ac7c2d093b02ed51a20ccf695623a5b75a
NerdScurvy <NerdScurvy@gmail.com>
2025-08-21 18:28:53 +0800

    Fix  ICarryEvent init

--- src/API/Common/ICarryEvent.cs
+++ src/API/Common/ICarryEvent.cs
@@ -1,10 +1,7 @@
-using Vintagestory.API.Common; // Add this if IModSystem is defined in Vintagestory.API.Common
-
 namespace CarryOn.API.Common
 {
     public interface ICarryEvent
     {
-      // TODO: Change to send API instead of ModSystem when API implements way to get CarryEvents
-      void Init(ModSystem modSystem);
+      void Init(ICarryManager carryManager);
     }
 }
