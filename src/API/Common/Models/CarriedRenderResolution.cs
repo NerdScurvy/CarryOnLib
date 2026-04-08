957dd1d82d27b4b275564c3ad3b9058cab54e513
NerdScurvy <NerdScurvy@gmail.com>
2026-04-08 09:00:03 +0800

    Refactor PrimaryGroup property to manage PrimaryGroupCandidates list for improved group handling

--- src/API/Common/Models/CarriedRenderResolution.cs
+++ src/API/Common/Models/CarriedRenderResolution.cs
@@ -22,7 +22,35 @@ namespace CarryOn.API.Common.Models
 
     public class CarriedRenderResolution
     {
-        public string PrimaryGroup { get; set; }
+        public IList<string> PrimaryGroupCandidates { get; set; } = new List<string>();
+
+        public string PrimaryGroup
+        {
+            get => PrimaryGroupCandidates != null && PrimaryGroupCandidates.Count > 0
+                ? PrimaryGroupCandidates[0]
+                : null;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    return;
+                }
+
+                if (PrimaryGroupCandidates == null)
+                {
+                    PrimaryGroupCandidates = new List<string>();
+                }
+
+                if (PrimaryGroupCandidates.Count == 0)
+                {
+                    PrimaryGroupCandidates.Add(value);
+                }
+                else
+                {
+                    PrimaryGroupCandidates[0] = value;
+                }
+            }
+        }
 
         public IList<CarriedGroupCandidateSet> AdditionalGroupCandidates { get; set; } = new List<CarriedGroupCandidateSet>();
 
